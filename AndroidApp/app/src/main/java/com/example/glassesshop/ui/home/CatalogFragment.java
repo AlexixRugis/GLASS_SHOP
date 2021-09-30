740533239cde5e98ac08807059b56656f11c533b
AlexixRugis <75169888+AlexixRugis@users.noreply.github.com>
2021-09-30 14:58:02 +0300

    android start

---
+++ AndroidApp/app/src/main/java/com/example/glassesshop/ui/home/CatalogFragment.java
@@ -0,0 +1,36 @@
+package com.example.glassesshop.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.glassesshop.databinding.FragmentCatalogBinding;
+
+public class CatalogFragment extends Fragment {
+
+    private FragmentCatalogBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CatalogViewModel catalogViewModel = new ViewModelProvider(this).get(CatalogViewModel.class);
+
+        binding = FragmentCatalogBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textCatalog;
+        catalogViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
