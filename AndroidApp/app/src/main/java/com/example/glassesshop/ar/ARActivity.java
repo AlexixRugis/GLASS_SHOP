2841353579074a4fe94d46cfa5b65c46f19c007a
AlexixRugis <75169888+AlexixRugis@users.noreply.github.com>
2021-12-09 15:32:54 +0300

    ff

--- AndroidApp/app/src/main/java/com/example/glassesshop/ar/ARActivity.java
+++ AndroidApp/app/src/main/java/com/example/glassesshop/ar/ARActivity.java
@@ -1,10 +1,10 @@
 package com.example.glassesshop.ar;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.glassesshop.R;
 
@@ -13,33 +13,116 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
-public class ARActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    private CameraBridgeViewBase openCvCameraView;
-    private CascadeClassifier cascadeClassifier;
-    private Mat grayscaleImage;
-    private int absoluteFaceSize;
+public class ARActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    // distance from camera to object(face) measured
+    private final double KNOWN_DISTANCE = 76.2;  // centimeter
+    // width of face in the real world or Object Plane
+    private final double KNOWN_WIDTH = 14.3;  // centimeter
+    private final double FOCAL_LENGTH = 1000;
+
+
+    private JavaCameraView javaCameraView;
+    private File cascFile;
+    private CascadeClassifier faceDetector;
+    private Mat mRgba, mGrey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_aractivity);
+
+        javaCameraView = (JavaCameraView)findViewById(R.id.view);
+
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, baseLoaderCallback);
+        }
+        else {
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+
+        javaCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+        mGrey = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mGrey.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Imgproc.cvtColor(inputFrame.rgba(), mRgba, Imgproc.COLOR_BGR2RGB);
+
+        double faceWidth = getFaceWidth(mRgba);
+
+        if (faceWidth != 0)
+        {
+
+            double distance = getDistance(FOCAL_LENGTH, KNOWN_WIDTH, faceWidth);
+
+            Imgproc.putText(mRgba, "D: " + String.valueOf(Math.round(distance)),
+                    new Point(50, 50), 3, 1, new Scalar(255, 0, 0, 255), 2);
+        }
+
+        return mRgba;
+    }
+
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                    initializeOpenCVDependencies();
-                    break;
+                case LoaderCallbackInterface.SUCCESS: {
+                    try{
+                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalcatface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        cascFile = new File(cascadeDir, "haarcascade_frontalface.xml");
+                        FileOutputStream os = new FileOutputStream(cascFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+
+                        if (faceDetector.empty()) {
+                            faceDetector = null;
+                        } else {
+                            cascadeDir.delete();
+                        }
+
+                        javaCameraView.enableView();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e("err", "Failed to load cascade. Exception thrown: " + e);
+                    }
+                } break;
+
                 default:
                     super.onManagerConnected(status);
                     break;
@@ -47,82 +130,30 @@ public class ARActivity extends Activity implements CameraBridgeViewBase.CvCamer
         }
     };
 
-    private void initializeOpenCVDependencies() {
+    private double getFocalLength(double measuredDistance, double realWidth, double widthInRfImage) {
+        double focalLengthValue = (widthInRfImage * measuredDistance) / realWidth;
+        return focalLengthValue;
+    }
 
-        try {
-            // Copy the resource into a temp file so OpenCV can load it
-            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
-            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
-            FileOutputStream os = new FileOutputStream(mCascadeFile);
+    private double getDistance(double focalLength, double realFaceWidth, double faceWidthInFrame) {
+        double distance = (realFaceWidth * focalLength) / faceWidthInFrame;
+        return distance;
+    }
 
+    private double getFaceWidth(Mat imgGrey) {
+        double faceWidth = 0;
 
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = is.read(buffer)) != -1) {
-                os.write(buffer, 0, bytesRead);
-            }
-            is.close();
-            os.close();
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(imgGrey, faceDetections, 1.3, 5);
 
-            // Load the cascade classifier
-            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-        } catch (Exception e) {
-            Log.e("OpenCVActivity", "Error loading cascade", e);
+        for (Rect rect : faceDetections.toArray()) {
+            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0));
+
+            faceWidth = rect.width;
         }
 
-        // And we are ready to go
-        openCvCameraView.enableView();
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        openCvCameraView = new JavaCameraView(this, -1);
-        setContentView(openCvCameraView);
-        openCvCameraView.setCvCameraViewListener(this);
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
-
-        // The faces will be a 20% of the height of the screen
-        absoluteFaceSize = (int) (height * 0.2);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat rgba = inputFrame.rgba();
-        // Create a grayscale image
-        Imgproc.cvtColor(inputFrame.rgba(), grayscaleImage, Imgproc.COLOR_RGBA2RGB);
-
-        MatOfRect faces = new MatOfRect();
-
-        // Use the classifier to detect faces
-        if (cascadeClassifier != null) {
-            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
-                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
-        }
-
-        // If there are any faces found, draw a rectangle around it
-        Rect[] facesArray = faces.toArray();
-        for (int i = 0; i <facesArray.length; i++)
-            Imgproc.rectangle(rgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
-
-        return rgba;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+        return faceWidth;
     }
 }
